a804775d7f0146267567fe84e02488b4c7f934dc
elishambadi <embadi43@gmail.com>
2023-01-26 10:11:20 +0300

    add task 2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
