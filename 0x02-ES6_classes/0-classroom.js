f38feadb052188475a8d3100a87de7358cb5252e
elishambadi <embadi43@gmail.com>
2023-01-26 14:11:18 +0300

    rename var

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this.maxStudentsSize = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize; // eslint-disable-line no-underscore-dangle
   }
 }
