a768d8fc42be3fdd51994be67ce025f6c35eb727
elishambadi <embadi43@gmail.com>
2023-01-26 10:36:04 +0300

    add backticks

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -6,7 +6,7 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return 'As of ${year}, it was the seventh-highest income county in the United States' // eslint-disable-line no-template-curly-in-string
-         / ', with a per capita personal income of ${budget.income}. As of 2015, San Francisco' // eslint-disable-line no-template-curly-in-string
-          / ' proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.'; // eslint-disable-line no-template-curly-in-string
+  return ` As of ${year}, it was the seventh-highest income county in the United States`
+        + `, with a per capita personal income of ${budget.income}. As of 2015, San Francisco`
+        + ` proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
