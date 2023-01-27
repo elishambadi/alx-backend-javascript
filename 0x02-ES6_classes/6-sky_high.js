4e381bc90a1c800d4153014dad960b94bd608233
elishambadi <embadi43@gmail.com>
2023-01-27 11:25:51 +0300

    add task

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors; // eslint-disable-line no-underscore-dangle
+  }
+
+  get floors() {
+    return this._floors; // eslint-disable-line no-underscore-dangle
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`; // eslint-disable-line no-underscore-dangle
+  }
+}
