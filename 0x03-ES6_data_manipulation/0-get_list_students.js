18199ee4899745f7959fecdb1708ec56e4270c95
elishambadi <embadi43@gmail.com>
2023-01-30 10:17:38 +0300

    init

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function getListStudents(){
+  let arr = [];
+  const student1 = {id: 1, firstName: 'Guillaume', location: 'San Francisco'};
+  const student2 = {id: 2, firstName: 'James', location: 'Columbia'};
+  const student3 = {id: 5, firstName: 'Serena', location: 'San Francisco'};
+  arr.push(student1);
+  arr.push(student2);
+  arr.push(student3);
+  return arr;
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,11 @@
+# Data Manipulation
+This repo holds tasks on data manipulation and simple data structures in C.
+The data structures covered are:
+
+<ul>
+  <li>Sets</li>
+  <li>Maps</li>
+  <li>WeakMaps</li>
+  <li>Arrays</li>
+  <li>Typed Arrays</li>
+</ul>
