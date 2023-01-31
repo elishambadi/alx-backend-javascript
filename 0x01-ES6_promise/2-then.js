e236e2ac9ca06618db804365f4046c595fbb20d7
elishambadi <embadi43@gmail.com>
2023-01-31 12:05:29 +0300

    fix task 2 promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,19 @@
+// export default function handleResponseFromAPI(promise) {
+//   return promise.then(() => ({
+//     status: 200,
+//     body: 'success',
+//   }))
+//     .catch(() => Error())
+//     .finally(console.log('Got a response from the API'));
+// }
+
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+export default handleResponseFromAPI;
