3048049607939195a6c1082dc41c47e65c81c842
elishambadi <embadi43@gmail.com>
2023-07-02 15:14:58 +0300

    inherit

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Amount must be a number.');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new Error('Currency must be an instance of Currency class.');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number.');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw new Error('Conversion Rate must be a number.');
+    }
+
+    return amount * conversionRate;
+  }
+}
