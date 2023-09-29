467358f8c417f3dd6047f067685620db8a96af58
elishambadi <35599354+elishambadi@users.noreply.github.com>
2023-09-29 19:09:14 +0300

    Create 0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Your code here
+    resolve(); // Resolve the Promise when done
+  });
+}
+
+export default getResponseFromAPI;
