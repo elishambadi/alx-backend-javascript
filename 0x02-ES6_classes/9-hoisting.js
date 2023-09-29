72e118fcf7b3e8cf54a410b1812f2f2b1c90ad8d
elishambadi <35599354+elishambadi@users.noreply.github.com>
2023-09-29 18:53:19 +0300

    Create 9-hoisting.js

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,36 @@
+class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+export { HolbertonClass, StudentHolberton };
